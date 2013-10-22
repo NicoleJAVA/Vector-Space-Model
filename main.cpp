c9d9a83a92770f81cc27f7724d373ed552f689ad
Nicole Chang <powernicole2@yahoo.com.tw>
2013-10-22 21:38:11 +0800

    Ver. 25 >> doctxtname.cpp
    Ver. 25 >> doctxtname.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 
-/* ver. 20 - main( )
+//		In Github local repository !
 
-1.) Ver. 20 
-stemByPorter[] 應該要從 i = 1 而非 i = 0 開始, 去做 insertNode ( )  
-Wrong code : 
-	for( i = 0; i <= tokenSize; i++ ){
-		insertNode( dictNode, stemByPorter[ i ], i );
-				
-Corrct Code :
-	for( i = 1; i <= tokenSize; i++ ){
 
-*/
+
+/******************		Ver. 25  -  main( )		*********************
+
+1.)	Nicole does N O T  want to write that sort-list-node STUFF ANYMORE !
+
+
+************************************************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -34,7 +34,7 @@ Corrct Code :
 #endif
 #define DICT_SIZE 1000/* dictionary size : total number of tokens of all docs.*/
 using namespace std;
-
+ListNode *dummyS; /*global dummy pointer for Sorting*/
 
 
 /*------------------------------------------------*/
@@ -43,49 +43,56 @@ int main()
 	FILE * stopFile;
 	FILE * aFile;
 	FILE * stemFile; /* pointer stemFile will be passed to poter( ) */
+	FILE * resultFile;
 	ListNode *dictNode; /* dictionary list node */
+	
+	std::string newsSourceTitle ;		//		const char * newsSourceTitle;	
+	std::string stemResultTitle ;		//		const char * stemResultTitle;	
+	std::string vectorTitle ;			//		const char * vectorTitle;
+	
    char stopArr[ MAX_STOP_SIZE ][ MAX_STR_LEN ];
    char article[ 10000 ][ MAX_STR_LEN ];
    char check[ 10000 ][ MAX_STR_LEN ];
    char stemByPorter[ 10000 ][ MAX_STR_LEN ];
-   /* PPS. Don't name the array as "porter" because it would be confused */
-   /* with "poter.cpp" and "poter( );"  */
-	char dict[ DICT_SIZE ][ MAX_STR_LEN ];
-	char **dptr = NULL; /* dictionary pointer */
-	dptr = (char**)malloc(sizeof(char*)*20);
+   // PPS. Don't name the array as "porter" because it would be confused 
+   // with "poter.cpp" and "poter( );"  
+   char dict[ DICT_SIZE ][ MAX_STR_LEN ];
+
 
    int stopSize = 0;
    int tokenSize = 0;
    int stemSize = 0;
    int currDictSize = 0;
    int i = 0;
-   
+
+
+
+						/****************************/
+						/****   open stopFile   ****/
+						/****************************/
+						
 	stopFile=fopen( "stopwordfile.txt", "r" );
+
    txtToArray( stopFile, stopArr, stopSize );    
- 
-							 
-							 
-							 	/* Testing arrayToListNode */
- 
- 
- 
-	printf("\n\n\n\n\n			------------------------------" );
-	printf("\n			Testing arrayToListNode\n\n\n\n\n" );	
-   
 	clear_array( article );
    rmvStop( article, stopArr, stopSize, tokenSize  );
-		/* PPS. Now rmvStop() resets tokenSize to zero and then updates tokenSize*/
-   //printf("\nrmvStop( ) done ! " ); 
-   
+	// After rmvStop(), rmvStop() first resets tokenSize to zero 
+	//and then updates tokenSize
+
+						/*********************************/
+						/****   loop every "xxx.txt"  ****/
+						/********************************/
+
 	aFile = fopen( "afile.txt" , "w" );
-   if ( aFile==NULL ) perror ( "\n In main() : File Cannot Be Opened.\n" );
-   else
+   if ( aFile==NULL ) perror ( "\n In main() : File Cannot Be Opened.\n" ); /*2-if-else*/
+   else //	2-if-else
    {		   
 		
 		clear_array( stemByPorter );
 		
-		/* 						->		write the stuff in article[][] into aFile */
-		while( i <= tokenSize ){ /* tokenSize has already been updated by rmvStop()*/
+		// while-loop to write the stuff in article[][] into aFile >>
+		// before enter while-loop, tokenSize has already been updated by rmvStop()
+		while( i <= tokenSize ){ 
 			fprintf( aFile, "%s ", article[ i ] );
 	 	  i++;  
 		} 							/* End 3-while */
@@ -94,117 +101,29 @@ int main()
 		/*--------------------------------------------------------*/
 		stemFile = fopen( "afile.txt" ,"r");
 		rewind(stemFile);
-  		porter( 1, stemByPorter, tokenSize, stemFile );
+  		porter( 1, stemByPorter, tokenSize, stemFile, resultFile );
   		fclose(stemFile);
   		
 		printf("After porter(), tokenSize is %d\n", tokenSize );
-  		
-	/* pre-condition : after porter() is done, stemByPorter[][] now */
-	/* holds the stemming result of afile.txt. */					
-	/* pre-condition : tokenSize is correctly updated*/					
-					
-					
-					/*              H E L L O                      */
+
 					
 					
 					
-	printf("\n先把字典變成 listnode"); 
-	/* Ver. 14 開始, deprecate 這行 : arrayToListNode( stemByPorter, tokenSize, dictNode ); */ 
-	/* Ver. 14 開始, deprecate 這行 : arrayToDict( stemByPorter, tokenSize, dictNode ); */
-	printf("先看看作業 1 的 stemByPorter[1] 是 %s.", stemByPorter[1] ); 
-	for( i = 1; i <= tokenSize; i++ ){
-		//printf("先看看作業 1 的 stemByPorter[i] 是 %s.", stemByPorter[i] ); 
-		insertNode( dictNode, stemByPorter[ i ], i );
-	}					/* end for */	
+
 
  		/*--------------------------------------------------------*/ 
    }  							/* END 2-if-else */
-   	printf("\n\n		 E N D - Testing arrayToListNode - E N D - \n\n\n\n\n" );	
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 							 	/*  E N D - Testing arrayToListNode - E N D - */
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-   
-for( int docLoop = 1; docLoop <= 0; docLoop ++ )
-{    
-	printf("\n\n\n\n\n------------------------------" );
-	printf("\ndocument %d.txt", docLoop );	
-   
-	clear_array( article );
-   rmvStop( article, stopArr, stopSize, tokenSize  );
-		/* PPS. Now rmvStop() resets tokenSize to zero and then updates tokenSize*/
-   printf("\nrmvStop( ) done ! " ); 
-   aFile = fopen( "afile.txt" , "w" );
-   printf("\nSuccessfully open file. aFile is now : %d", aFile);
-   printf("\naFile = fopen afile.txt, w" );
-   if ( aFile==NULL ) perror ( "\n In main() : File Cannot Be Opened.\n" );
-   else
-   {		   
-		
-		clear_array( stemByPorter );
-		
-		/* 						->		write the stuff in article[][] into aFile */
-		while( i <= tokenSize ){ /* tokenSize has already been updated by rmvStop()*/
-			fprintf( aFile, "%s ", article[ i ] );
-	 	  i++;  
-		} 							/* End 3-while */
-		fclose( aFile );
-		printf("\nIn main( ) : fclose aFile." );
-		/*--------------------------------------------------------*/
-		stemFile = fopen( "afile.txt" ,"r");
-		rewind(stemFile);
-  		porter( 1, stemByPorter, tokenSize, stemFile );
-  		fclose(stemFile);
-  		printf("In main( ) : porter is done.\n And tokenSize is %d\n", tokenSize );
+  
 
+ 
+ 
 
+ 
+ 
+ 
+ 
+ 
 
-					/* - - - - - - - - - - - - - - - */
-							
-							
-								/* dictCpp.cpp*/
-								
-								
-								
-					/* - - - - - - - - - - - - - - - */
-					
-					
-	/* pre-condition : after porter() is done, stemByPorter[][] now */
-	/* holds the stemming result of afile.txt. */					
-	/* pre-condition : tokenSize is correctly updated*/					
-					
-				
-					
-					
-					/* - - - - - - - - - - - - - - - */
-
-
-
-
-  		/*--------------------------------------------------------*/ 
-   }  							/* END 2-if-else */
-} 									/* End 1-for */
 
 	printf( "\n\n\n\n\n\n\n" );
 	printf( "          -------------\n" );
