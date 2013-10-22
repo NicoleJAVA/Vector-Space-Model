2c8c37c4e18cab181db1d8b27ff3e0a7b5bb2496
Nicole Chang <powernicole2@yahoo.com.tw>
2013-10-22 09:11:47 +0800

    Ver. 16 compareTerm.cpp
    Ver. 16 compareTerm.cpp
    /* Ver. 16- compareTerm.cpp
    
    1.)
    Don't use the official strncmp/strcmp anymore.
    Why not write a compare_term( ) function yourself?
    別用官方的 strncmp，何不乾脆自己寫一個 compare_term( ) ?
    
    2.)
    [結果]
    strlen(s1) = 5
    strlen(s2) = 5
    [說明]
    strlen():回傳字串的長度("不"包括terminated null character)
    
    3.)
    int flag_same_string = 2;
    // 注意 ! 不是指整個字串都一樣, 而是指目前有比較到的字串都一樣
    //*先假設他們是完全一樣的字串
    
    */

--- compareTerm.cpp
+++ compareTerm.cpp
@@ -1,5 +1,5 @@
 
-/* Ver. 15	-	compareTerm.cpp
+/* Ver. 16-	compareTerm.cpp
 
 1.)
 Don't use the official strncmp/strcmp anymore.
@@ -12,6 +12,11 @@ strlen(s1) = 5
 strlen(s2) = 5
 [說明]
 strlen():回傳字串的長度("不"包括terminated null character)
+
+3.)
+   int flag_same_string = 2; 
+	// 注意 ! 不是指整個字串都一樣, 而是指目前有比較到的字串都一樣 
+	//*先假設他們是完全一樣的字串 
  
  */
 
@@ -38,8 +43,11 @@ int compare_term( const char *inputStr1, const char *inputStr2  )
 {
    int i = 0; 
    int flag = 1 ;
-   int flag_same_string = 2; /* 先假設他們是完全一樣的字串 */
+   int flag_same_string = 2; 
+	/* 注意 ! 不是指整個字串都一樣, 而是指目前有比較到的字串都一樣 */ 
+	/* 先假設他們是完全一樣的字串 */
    int flag_same_next_ch = 2; /* 先假設他們的下一個字元也是同樣的*/
+
 	char term1[ MAX_STR_LEN ];
 	char term2[ MAX_STR_LEN ];
   	char tempchar1;
@@ -51,18 +59,25 @@ int compare_term( const char *inputStr1, const char *inputStr2  )
 	printf("\nHello compare_term >> term1 is >> %s.", term1 );
 	printf("\nHello compare_term >> term2 is >> %s.", term2 );
 	
+
+	
    for( i = 0; term1[i] != '\0' && term2[i] != '\0' ; i++ ){
 		printf("\n\n\n進入 %d 迴圈\n", i ); 
 		if( term1[i] > term2[i] ){
-			flag = 0;
+			
+			if(flag_same_string == 2)
+			{
+				flag = 0;
+				printf("\n從現在開始,flag 變成 0 了 " ); 
+			}
 			flag_same_string = -1;
-			printf("\n旗子變成 0 了" ); 
 		}				// end if
+		
 		if( term1[i] < term2[i] ){
 			/* 不要改變 flag 的值 */
-			printf("\n還沒開始改變 flag 的值. >> the value of flag hasn't been alterd yet." ); 
 			flag_same_string = -1;
 		}				// end if		
+		
 		if( term1[i] == term2[i] ){
 			printf("\n兩字元相同" ); 
 		}
@@ -70,7 +85,7 @@ int compare_term( const char *inputStr1, const char *inputStr2  )
 		printf("\n目前 flag是 %d. ", flag );
 		printf("\n目前 flag_same_string 是 %d. ", flag_same_string ); 
 		if( term1[ i+1 ] != term2[ i+1 ] ){
-			if( flag_same_next_ch == 2 ){
+			if( flag_same_string == 2 ){
 				printf("\n注意 -----> 從下一個字元開始, 兩字串開始不一樣了" ); 
 			}
 		}
@@ -79,21 +94,35 @@ int compare_term( const char *inputStr1, const char *inputStr2  )
 		//printf("\nterm1 : %c. term2 : %c.", tempchar1, tempchar2 );
 		//printf("\nIn compare_term() : flag is : %d.\n", flag );
 		
+						/* --- 以下是處理長短不一的字串 --- */
+		
 		if( term1[ i+1 ] =='\0' && term2[ i+1 ] != '\0' ){
+			flag_same_string = -1;
 			if( flag_same_string == 2 ){
 				flag = 1;
 				printf("\n\nterm2 比 term1 長. flag 為 %d", flag );
 			}
 			
 		}
+		
 		if( term1[ i+1 ] !='\0' && term2[ i+1 ] == '\0' ){
-			printf("\nterm2 比 term1 長."); 
+			flag_same_next_ch = -1;
+			if( flag_same_string == 2 ){
+				
+				printf("\nterm1 比 term2 長."); 
+			}				/* end if */
 		}
 		
 		
 		
 	}					/* end for */
-	return flag;
+	
+	if( flag_same_string == 2 && strlen(inputStr1) == strlen(inputStr2) ){
+		return 2;
+	}
+	else{
+		return flag;
+	}
 }						/* end compare_term( )  */
 
 /*
@@ -114,14 +143,26 @@ int main(){
 */
 int main()
 {
-	int cmpResult; /* compare result of compare_term( ) */
-	std::string strA = "atloooss";
-	std::string strB = "aHeoool";	
+	int cmpResultAB; /* compare result of compare_term( ) */
+	int cmpResultCD; /* compare result of compare_term( ) */
+	int cmpResultEF; /* compare result of compare_term( ) */
+	std::string strA = "abzssss";
+	std::string strB = "ahc";	
+	std::string strC = "abzssss";
+	std::string strD = "abcss";	
+	std::string strE = "abzsssss";
+	std::string strF = "abzss";	
 	
 	printf("\n字串的長度到底是多少呢 : %d.", strlen( strA.c_str() ) ); 
+	printf("\n字串的長度到底是多少呢 : %d.", strlen( strC.c_str() ) ); 
+	//cmpResultAB = compare_term( strA.c_str(), strB.c_str() );	
+	cmpResultCD = compare_term( strC.c_str(), strD.c_str() );
+	//cmpResultEF = compare_term( strE.c_str(), strF.c_str() );	
 	
-	cmpResult = compare_term( strA.c_str(), strB.c_str() );	
-	printf("\n\ncmpResult 的結果是 : %d.\n\n", cmpResult ); 
+	//printf("\n\nA 與 B 的cmpResult 的結果是 : %d.", cmpResultAB ); 
+	printf("\n\nC 與 D 的cmpResult 的結果是 : %d.", cmpResultCD ); 
+	//printf("\n\nE 與 F 的cmpResult 的結果是 : %d.", cmpResultEF ); 
 	
+	printf("\n\n\n");
 	system("PAUSE");
 }
